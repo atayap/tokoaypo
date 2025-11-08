27c1abb62f5241bc994e961bbecc75a2d97b724f
Atya-web678 <atayafikri624@gmail.com>
2025-11-09 01:26:01 +0700

    Aypo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 // src/pages/Home.js
 import React, { useEffect, useRef } from 'react';
 import './Home.css';
-import logo from '../public/logo.png';
+import logo from '../pages/logo.png';
 
 // Hook untuk animasi scroll yang optimal
 const useScrollAnimation = () => {
@@ -68,7 +68,7 @@ const Home = ({ isBlurred }) => {
         <div className="hero-content">
           <div className="hero-logo">
             <img 
-              src="{process.env.PUBLIC_URL + '/logo.png'}" 
+              src={process.env.PUBLIC_URL + '/logo.png'} 
               alt="Ayam Potong Segar Logo" 
               className="favicon-image"
             />
